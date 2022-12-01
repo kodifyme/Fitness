cd32d1b1767659d92ea9bfb396100dbd6d7ad3cd
Nikolay Dobychin <koddek@bk.ru>
2022-12-01 16:32:17 +0300

    collectionView and collectionViewCell created

--- Fitness/Views/MainVC/CalendarView.swift
+++ Fitness/Views/MainVC/CalendarView.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class CalendarView: UIView {
     
+    private let idCalendarCell = "idCalendarCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 3
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .none
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -24,12 +35,49 @@ class CalendarView: UIView {
         backgroundColor = .specialGreen
         layer.cornerRadius = 10
         translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(collectionView)
+        collectionView.register(CalendarCollectionViewCell.self, forCellWithReuseIdentifier: idCalendarCell)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension CalendarView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: idCalendarCell, for: indexPath) as? CalendarCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension CalendarView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("tapCollectionCell")
+    }
+}
+
+extension CalendarView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width / 8,
+               height: collectionView.frame.height)
     }
 }
 
 extension CalendarView {
-    
     private func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 105),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
     }
 }
+
+
---
+++ Fitness/Views/MainVC/Cells/CalendarCollectionViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  CalendarCollectionViewCell.swift
+//  Fitness
+//
+//  Created by KODDER on 01.12.2022.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    private let dayOfWeekLabel: UILabel = {
+        let label = UILabel()
+        label.text = "We"
+        label.font = .robotoBold16()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let numberOfWeekLabel: UILabel = {
+        let label = UILabel()
+        label.text = "07"
+        label.font = .robotoBold20()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        layer.cornerRadius = 10
+        addSubview(dayOfWeekLabel)
+        addSubview(numberOfWeekLabel)
+    }
+}
+
+extension CalendarCollectionViewCell {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            dayOfWeekLabel.topAnchor.constraint(equalTo: topAnchor, constant: 7),
+            dayOfWeekLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            numberOfWeekLabel.topAnchor.constraint(equalTo: dayOfWeekLabel.bottomAnchor, constant: 5),
+            numberOfWeekLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
