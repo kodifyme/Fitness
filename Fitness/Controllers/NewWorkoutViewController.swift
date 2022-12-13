5333d9468c589386f2a384e1d779d6e94d847bab
Nikolay Dobychin <koddek@bk.ru>
2022-12-13 19:04:45 +0300

    Create NewWorkoutViewController

---
+++ Fitness/Controllers/NewWorkoutViewController.swift
@@ -0,0 +1,100 @@
+//
+//  NewWorkoutViewController.swift
+//  Fitness
+//
+//  Created by KODDER on 13.12.2022.
+//
+
+import UIKit
+
+class NewWorkoutViewController: UIViewController {
+
+    private let mainLabel = UILabel(text: "NEW WORKOUT", font: .robotoMedium24(), textColor: .specialGray)
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "closeButton")!.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nameLabel = UILabel(text: "Name")
+    
+    private let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .specialBrown
+        textField.layer.cornerRadius = 10
+        textField.textColor = .specialGray
+        textField.font = .robotoBold20()
+        textField.leftView = UIView(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: 10,
+                                                  height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .done
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        setConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .specialBackground
+        
+        view.addSubview(mainLabel)
+        view.addSubview(closeButton)
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+    }
+    
+    @objc private func closeButtonTapped() {
+        print("close")
+    }
+}
+
+extension NewWorkoutViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            
+            nameLabel.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 5),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 0),
+            nameTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
