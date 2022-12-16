e50d10200a4b7ad15f928b3e88ccbeb5ab32726a
Nikolay Dobychin <koddek@bk.ru>
2022-12-16 23:38:06 +0300

    Settings Realm

---
+++ Fitness/Models/WorkoutModel.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutModel.swift
+//  Fitness
+//
+//  Created by KODDER on 16.12.2022.
+//
+
+import UIKit
+import RealmSwift
+
+class WorkoutModel: Object {
+    @Persisted var workoutDate: Date
+    @Persisted var workoutNumberOfDay: Int = 0
+    @Persisted var workoutName: String = "Unknow"
+    @Persisted var workoutRepeat: Bool = true
+    @Persisted var workoutSets: Int = 0
+    @Persisted var workoutReps: Int = 0
+    @Persisted var workoutTimer: Int = 0
+    @Persisted var workoutImage: Data?
+    @Persisted var workoutStatus: Bool = false
+}
---
+++ Fitness/Service/RealmManager.swift
@@ -0,0 +1,23 @@
+//
+//  RealmManager.swift
+//  Fitness
+//
+//  Created by KODDER on 16.12.2022.
+//
+
+import UIKit
+import RealmSwift
+
+class RealmManager {
+    
+    static let shared = RealmManager()
+    private init() {}
+    
+    let localRealm = try! Realm()
+    
+    func saveWorkoutModel(model: WorkoutModel) {
+        try! localRealm.write{
+            localRealm.add(model)
+        }
+    }
+}
